f3bc95f7eb5183d59db69198394dbd42dba9a1de
Andrey Rybin <RybinAV6.transport.mos.ru>
2025-12-08 23:02:07 +0300

    fix: webots driver

--- src/robot/robot/webots_driver.py
+++ src/robot/robot/webots_driver.py
@@ -3,7 +3,6 @@ from .base_driver import BaseDriver
 
 class WebotsDriver(BaseDriver):
     def init(self, robot_node, properties):
-        # Don't call super().__init__() here as it's already called by BaseDriver
         self.__robot = robot_node.robot
 
         self._left_motor = self.__robot.getDevice('left wheel motor')
@@ -18,5 +17,5 @@ class WebotsDriver(BaseDriver):
     def velocity_to_motors(self, command_motor_left, command_motor_right):
         # Check if motors are initialized before using them
         if hasattr(self, '_left_motor') and hasattr(self, '_right_motor'):
-            self._left_motor.setVelocity(command_motor_left)
-            self._right_motor.setVelocity(command_motor_right)
+            self._left_motor.setVelocity(-command_motor_left)
+            self._right_motor.setVelocity(-command_motor_right)
